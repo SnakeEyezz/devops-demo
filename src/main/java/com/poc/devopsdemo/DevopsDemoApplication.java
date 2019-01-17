c6ff06a685e62b868e9c936442a6c4020ff9230c
Prakash <anshprakash@deloitte.com>
2019-01-17 11:01:08 +0530

    initial commit

---
+++ src/main/java/com/poc/devopsdemo/DevopsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.poc.devopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoApplication.class, args);
+	}
+
+}
+
